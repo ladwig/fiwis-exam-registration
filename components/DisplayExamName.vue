659bf2a3234dea739d8531bc8dbe5422f97999ac
Daniel Ladwig <ladwig.daniel@icloud.com>
2021-04-06 15:15:56 +0200

    Added DisplayExamName comp

--- components/DisplayExamName.vue
+++ components/DisplayExamName.vue
@@ -1,19 +1,28 @@
 <template>
   <v-container>
-    {{ examName }}
+    <h3 v-if="examInProgress" class="display-2">{{ examName }}</h3>
+    <h3 v-else class="display-2">Derzeit keine Prüfung in diesem Raum</h3>
+    <v-btn @click="toggle">Test</v-btn>
   </v-container>
 </template>
 
 <script>
-import {mapState} from "vuex";
+import {mapMutations, mapState} from "vuex";
 
 export default {
   name: "DisplayExamName",
   computed: {
     ...mapState({
-      examName: state => state.exam.examName
+      examName: state => state.exam.examName,
+      examInProgress: state => state.examInProgress
     })
+
   },
+  methods: {
+    ...mapMutations({
+      toggle: 'toggle' // To set examInProgress to true/false for testing
+    })
+  }
 
 }
 </script>
--- store/index.js
+++ store/index.js
@@ -1,13 +1,19 @@
 export const state = () => ({
-  date: null,
-  roomName: null,
-  checkMode: true,
-  noExam: true,
-  cardNumber: null,
-})
+    date: null,
+    roomName: null,
+    checkMode: true,
+    examInProgress: false,
+    cardNumber: null,
+  }
+)
 
 export const mutations = {
   setRoomName(state, roomName) {
     state.roomName = roomName;
   },
+
+
+  toggle(state, examInProgress) {
+    state.examInProgress = !state.examInProgress
+  }
 }
