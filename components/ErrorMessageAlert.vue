c22385d9e85ee3cbbf7d8ae1d655943296632d49
Daniel Ladwig <ladwig.daniel@icloud.com>
2021-04-27 18:24:30 +0200

    Added first version of time after exam

---
+++ components/ErrorMessageAlert.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-row justify="center">
+    <v-dialog
+      v-model="dialog"
+      persistent
+      max-width="400"
+    >
+      <v-card>
+        <v-card-title class="headline">
+          Prüfungsregistrierung starten
+        </v-card-title>
+        <v-card-text>Starten Sie die Prüfungsregistierung, sodass Studierende ihre Anwesenheit ab sofort bestätigen
+          können.
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="secondary darken-4"
+            text
+            @click="closePopup(0)"
+          >
+            Abbrechen
+          </v-btn>
+          <v-btn
+            color="primary"
+            @click="closePopup(1)"
+          >
+            Starten
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+import {mapMutations} from "vuex";
+
+export default {
+  name: "StartExamRegisterAlert",
+  data() {
+    return {
+      dialog: true
+    }
+  },
+  methods: {
+    ...mapMutations([
+        "setIsExaminer",
+        "setModeExamRegister",
+      ]
+    ),
+    //It will always close the modal and removes examiner data. Case 1 -> Starts exam register mode
+    closePopup(action) {
+      if (action == 1) {
+        this.setModeExamRegister(true)
+      }
+      this.setIsExaminer(false)
+      this.dialog = false
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
