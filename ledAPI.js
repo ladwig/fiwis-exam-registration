c0815ebd769db9fc16f112c2a07e8bdee18fed01
Daniel Ladwig <ladwig.daniel@icloud.com>
2021-06-13 22:37:00 +0200

    LEDs only on android/tablet device

--- ledAPI.js
+++ ledAPI.js
@@ -1,28 +1,29 @@
 export function changeColor(hex) {
-
-  const access = {
-    base: 'http://localhost:8080/v2',
-    token: ''
-  }
-  const xhr = new XMLHttpRequest()
-  const data = {
-    id: 0,
-    name: 'frame',
-    brightness: 1.0,
-    color: hex
-  }
-  let data_string = '';
-  for(let key in data) {
-    data_string += '&'+key+(data[key] ?'='+data[key] :'');
-  }
-  xhr.open('POST', access.base+'/hardware/light?access_token='+access.token, true);
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-  xhr.onreadystatechange = function() {
-    if (this.readyState == 4) {
-      if (this.status != 200) {
-        alert(this.status);
+  if(/Android|webOS/i.test(navigator.userAgent)) {
+    const access = {
+      base: 'http://localhost:8080/v2',
+      token: ''
+    }
+    const xhr = new XMLHttpRequest()
+    const data = {
+      id: 0,
+      name: 'frame',
+      brightness: 1.0,
+      color: hex
+    }
+    let data_string = '';
+    for (let key in data) {
+      data_string += '&' + key + (data[key] ? '=' + data[key] : '');
+    }
+    xhr.open('POST', access.base + '/hardware/light?access_token=' + access.token, true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    xhr.onreadystatechange = function () {
+      if (this.readyState == 4) {
+        if (this.status != 200) {
+          alert(this.status);
+        }
       }
     }
+    xhr.send(data_string);
   }
-  xhr.send(data_string);
 }
